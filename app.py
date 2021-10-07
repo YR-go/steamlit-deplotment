1544d79fd4bf9f1da076ee11a37205ca359fcc51
gokkiri <shinyl2003@naver.com>
2021-10-07 15:55:36 +0900

    no message

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats, parkingLot, CafeAPP
+from apps import home, data_stats, parkingLot, cafeAPP
 
 app = MultiApp()
 
@@ -8,6 +8,6 @@ app = MultiApp()
 app.add_app("HOME", home.app)
 app.add_app("Data Analysis", data_stats.app)
 app.add_app('AI Parking Lot System', parkingLot.app)
-app.add_app("Cafe App with NFC", CafeAPP.app)
+app.add_app("Cafe App with NFC", cafeAPP.app)
 # The main app
 app.run()
