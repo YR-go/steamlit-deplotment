e5af08335c7e23427f36570cde9c04e6cd58f030
gokkiri <shinyl2003@naver.com>
2021-10-07 16:41:36 +0900

    no message

--- apps/analapp/대중교통.py
+++ apps/analapp/대중교통.py
@@ -4,19 +4,6 @@ import matplotlib.pyplot as plt
 import streamlit as st
 from matplotlib import font_manager, rc
 base = "assets/"
-import platform
-
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
-
-if platform.system() == 'Darwin':
-    rc('font', family='AppleGothic')
-elif platform.system() == 'Windows':
-    path = "c:/Windows/Fonts/malgun.ttf"
-    font_name = font_manager.FontProperties(fname=path).get_name()
-    rc('font', family=font_name)
-else:
-    print('Unknown system... sorry~~~~')
 
 def app():
 
